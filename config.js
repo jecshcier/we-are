3dc2e3f9e0dfd1ddb74f9486b368f614949633d9
jecshcier <alsk123111@icloud.com>
2018-03-28 14:44:55 +0800

    update

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ module.exports = {
         timestamps: false // true by default
       },
       timezone: "+08:00",
-      host:"192.168.109.236",
-      // host: "127.0.0.1",
+      // host:"192.168.109.236",
+      host: "127.0.0.1",
       dialect: "mysql",
       dialectOptions: {
         charset: "utf8mb4"
